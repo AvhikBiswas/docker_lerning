664c9f0e80358c59b61b12524d227f84d062d5e0
AvhikBiswas <abhikbiswas351@gmail.com>
2024-04-28 01:36:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const port=8000;
+
+app.get('/',(req,res)=>{
+    req.status(201).json({
+        mess:'server running',
+        err:false,
+    })
+})
+
+app.listen(port,()=>{
+    console.log('server running on '+ port);
+})
